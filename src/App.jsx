9685ad87be4ab2494d7573d21f5a1cb68a004a4f
Mannan <kocharmanan88@gmail.com>
2025-08-15 03:15:59 +0530

    hash routing for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import Home from './screens/Home';
 import './App.css';
 import PokemonInfo from './screens/PokemonInfo';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
-  return(
+  return (
     <div>
-   <Router>
+      <Router>
         <Routes>
-           <Route path="/" element={<Home />} />
-            <Route path="/:pokename" element={<PokemonInfo />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/:pokename" element={<PokemonInfo />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
 export default App;
